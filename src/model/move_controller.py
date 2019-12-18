7c1e87add5e6d90bf5573994275311f366f041f4
Robbie McVickers <robertjamesmcvickers@gmail.com>
2019-12-18 15:44:04 +0000

    move controller handles different move types

---
+++ src/model/move_controller.py
@@ -0,0 +1,31 @@
+import move as mv
+
+
+def make_move(board, player_colour, start_point, end_point):
+    is_player_on_bar = board.is_player_on_bar(player_colour)
+    if is_player_on_bar:
+        return mv.enter(board, player_colour, end_point)
+
+    _is_player_home = is_player_home(board, player_colour)
+    if _is_player_home:
+        if end_point == -1:
+            return mv.bear_off(board, start_point)
+        else:
+            return mv.move(board, player_colour, start_point, end_point)
+
+    is_standard = not is_player_on_bar and not _is_player_home
+    if is_standard:
+        return mv.move(board, player_colour, start_point, end_point)
+
+
+def is_player_home(board, player_colour):
+    sum = 0
+    if player_colour == "y":
+        for point_index in range(6):
+            if board.is_point_players(player_colour, point_index):
+                sum += board.get_num_checkers(point_index)
+    elif player_colour == "p":
+        for point_index in range(18, 24):
+            if board.is_point_players(player_colour, point_index):
+                sum += board.get_num_checkers(point_index)
+    return sum == 15
